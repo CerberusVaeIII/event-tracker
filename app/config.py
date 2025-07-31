c396aca2999d19cf6ee4d983375d88a6ac5ba0d5
CerberusVaeIII <rhatos2@gmail.com>
2025-07-31 21:48:08 +0300

    Variable path try2

--- app/config.py
+++ app/config.py
@@ -5,19 +5,21 @@ import os
 #Set up Jinja templates for HTML
 templates = Jinja2Templates(directory="templates")
 
-BASE_URL = os.getenv("ORIGIN", "http://localhost:8000")
+class Settings(BaseSettings):
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: str
+    origin: str = "http://localhost:8000"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+BASE_URL = settings.origin
 
 def render_template(request: Request, template_name: str, context: dict = {}):
     base_context = {"request": request, "base_url": BASE_URL}
     base_context.update(context)
     return templates.TemplateResponse(template_name, base_context)
 
-class Settings(BaseSettings):
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: str
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
